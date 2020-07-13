4646feb73b6afba5034c86bd61d7253da13b87bd
lww <1013232063@qq.com>
2020-07-13 18:30:07 +0800

    2020年7月13日18:30:01

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/CmsPageService.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/CmsPageService.java
@@ -7,7 +7,10 @@ import com.xuecheng.framework.model.response.QueryResponseResult;
 import com.xuecheng.framework.model.response.QueryResult;
 import com.xuecheng.manage_cms.dao.CmsPageRepository;
 import com.xuecheng.manage_cms.service.impl.CmsPageServiceImpl;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -27,6 +30,28 @@ public class CmsPageService implements CmsPageServiceImpl {
      */
     @Override
     public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest) {
+        if (queryPageRequest == null){
+            queryPageRequest = new QueryPageRequest();
+        }
+        CmsPage cmsPage = new CmsPage();
+
+        //精确查找(站点id)
+        if (StringUtils.isNotEmpty(queryPageRequest.getSiteId())){
+            cmsPage.setSiteId(queryPageRequest.getSiteId());
+        }
+        //精确查找（模板ID）
+        if(StringUtils.isNotEmpty(queryPageRequest.getTemplateId())){
+            cmsPage.setTemplateId(queryPageRequest.getTemplateId());
+        }
+        //模糊查找（要配合ExampleMatcher使用）
+        if (StringUtils.isNotEmpty(queryPageRequest.getPageAliase())){
+            cmsPage.setPageAliase(queryPageRequest.getPageAliase());
+        }
+        //自定义条件匹配器
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching().withMatcher("pageAliase", ExampleMatcher.GenericPropertyMatchers.contains());
+        //定义条件对象Example（就是把条件匹配器和实体关联起来）
+        Example<CmsPage> example = Example.of(cmsPage, exampleMatcher);
+
         //对外提供第一页为1
         if (page <= 0){
             page = 1;
@@ -36,7 +61,10 @@ public class CmsPageService implements CmsPageServiceImpl {
             size = 10;
         }
         PageRequest pageable = PageRequest.of(page, size);
-        Page<CmsPage> cmsPages = cmsPageRepository.findAll(pageable);   //返回的是一个Page
+        // 分页查询
+        //Page<CmsPage> cmsPages = cmsPageRepository.findAll(pageable);   //返回的是一个Page
+        // 分页、条件查询这些都不用自己写SpringData封装好了
+        Page<CmsPage> cmsPages = cmsPageRepository.findAll(example,pageable);   //返回的是一个Page
         QueryResult queryResult = new QueryResult();
         queryResult.setList(cmsPages.getContent());
         queryResult.setTotal(cmsPages.getTotalElements());
